bff71e45dcd03b608f2248d39e335f97ccc16280
LordFalcon787 <thehighking787@gmail.com>
2025-02-25 22:01:53 -0800

    Fixed.

--- donations/donosticky.py
+++ donations/donosticky.py
@@ -1,5 +1,7 @@
+import asyncio
 import nextcord
-from nextcord.ext import commands
+from nextcord.ext import commands, tasks
+from datetime import datetime, timedelta
 AUTO = [1267527896218468412, 1267530934723281009]
 
 class Donosticky(commands.Cog):
@@ -9,6 +11,42 @@ class Donosticky(commands.Cog):
     @commands.Cog.listener()
     async def on_ready(self):
         print("Donosticky cog loaded.")
+        self.messages_task.start()
+        
+    @tasks.loop(hours=3)
+    async def messages_task(self):
+        await self.clean_old_messages()
+
+    async def clean_old_messages(self):
+        time_threshold = datetime.now() - timedelta(hours=12)
+        for channel_id in AUTO:
+                channel = self.bot.get_channel(int(channel_id))
+                if channel:
+                    try:                        
+                        try:
+                            async with asyncio.timeout(120):
+                                messages = [msg async for msg in channel.history(limit=None) 
+                                          if msg.created_at < time_threshold and not msg.pinned]
+                        except asyncio.TimeoutError:
+                            messages = []
+                        await self.check_old_messages(messages)
+                    except:
+                        pass
+
+    async def check_old_messages(self, messages):
+        for message in messages:
+            try:
+                if message.author.id != self.bot.user.id:
+                    pass
+                elif not message.embeds:
+                    pass
+                elif not message.embeds[0].title:
+                    pass
+                if "completed" in message.embeds[0].title.lower():
+                    await message.delete()
+                    await asyncio.sleep(1)
+            except:
+                pass
 
     @commands.Cog.listener()
     async def on_message(self, message):
