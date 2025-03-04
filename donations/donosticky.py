013007188882d5beffa979fec35328c3af732e47
LordFalcon787 <thehighking787@gmail.com>
2025-03-04 10:57:32 -0800

    Updated logging.

--- donations/donosticky.py
+++ donations/donosticky.py
@@ -18,6 +18,7 @@ class Donosticky(commands.Cog):
         await self.clean_old_messages()
 
     async def clean_old_messages(self):
+        time_threshold = int(datetime.now().timestamp()) - 43200
         for channel_id in AUTO:
                 channel = self.bot.get_channel(int(channel_id))
                 if channel:
@@ -26,19 +27,21 @@ class Donosticky(commands.Cog):
                         try:
                             async with asyncio.timeout(900):
                                 print("Getting messages")
-                                messages = [msg async for msg in channel.history(limit=100)]
+                                messages = [msg async for msg in channel.history(limit=100) if msg.edited_at.timestamp() < time_threshold]
                         except asyncio.TimeoutError:
                             print("Timeout error")
                             messages = []
                         print(f"Found {len(messages)} messages to potentially clean")
-                        await self.check_old_messages(messages)
-                        print(f"Cleaned {len(messages)} messages from {channel.name}")
+                        if len(messages) > 0:
+                            await self.check_old_messages(messages, channel)
+                        else:
+                            print(f"No messages to clean from {channel.name}")
                     except Exception as e:
                         print(f"Error cleaning {channel.name}: {str(e)}")
-        print("Done cleaning messages")
+        print("Completed cleaning messages.")
 
-    async def check_old_messages(self, messages):
-        time_threshold = int(datetime.now().timestamp()) - 43200
+    async def check_old_messages(self, messages, channel):
+        cleaned = 0
         for message in messages:
             try:
                 if message.author.id != self.bot.user.id:
@@ -47,16 +50,14 @@ class Donosticky(commands.Cog):
                     continue
                 elif not message.embeds[0].title:
                     continue
-                elif not message.edited_at:
-                    continue
-                elif message.edited_at.timestamp() > time_threshold:
-                    continue
                 elif "completed" in message.embeds[0].title.lower() or "denied" in message.embeds[0].title.lower():
                     print(f"Deleting message {message.id} from {message.channel.name}")
                     await message.delete()
+                    cleaned += 1
                     await asyncio.sleep(5)
             except Exception as e:
                 print(f"Error checking message {message.id}: {str(e)}")
+        print(f"Cleaned {cleaned} messages from {channel.name}")
 
     @commands.Cog.listener()
     async def on_message(self, message):
