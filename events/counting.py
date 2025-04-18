c426330139161edf1fb42b0a99dcf3270a30bb07
LordFalcon787 <thehighking787@gmail.com>
2025-04-18 12:13:29 -0700

    Fixed asyncio lock error.

--- events/counting.py
+++ events/counting.py
@@ -53,44 +53,44 @@ class Counting(commands.Cog):
             return
         try:
             if len(message.content.split(" ")) == 1:
-                async with self.lock:
-                    await self.count(message)
+                await self.count(message)
             else:
                 return
         except:
             return
     
     async def count(self, message):
-        msg = message.content.replace(",", "")
-        msg = msg.replace(" ", "")
-        try:
-            num = int(msg)
-        except:
-            return
-        last_count = self.number_cache.get(message.guild.id, {}).get("number", 0)
-        last_counter = self.number_cache.get(message.guild.id, {}).get("last", 11)
-        if num <= last_count:
-            await message.channel.send(f"{RED_X} {message.author.mention} RUINED IT at {num}!! The next number is **1**.")
-            await message.add_reaction(RED_X)
-            self.number_cache[message.guild.id] = {"number": 0, "last": 11}
-            collection.update_one({"_id": f"current_count_{message.guild.id}"}, {"$set": {"Number": 0, "Last": 11}})
-            return
-        elif message.author.id == last_counter:
-            await message.channel.send(f"{RED_X} {message.author.mention} RUINED IT at {num}!! You cannot count twice in a row! The next number is **1**.")
-            self.number_cache[message.guild.id] = {"number": 0, "last": 11}
-            await message.add_reaction(RED_X)
-            collection.update_one({"_id": f"current_count_{message.guild.id}"}, {"$set": {"Number": 0, "Last": 11}})
-            return
-        elif num - last_count > 1:
-            await message.channel.send(f"{RED_X} {message.author.mention} RUINED IT at {num}!! You cannot skip numbers! The next number is **1**.")
-            self.number_cache[message.guild.id] = {"number": 0, "last": 11}
-            await message.add_reaction(RED_X)
-            collection.update_one({"_id": f"current_count_{message.guild.id}"}, {"$set": {"Number": 0, "Last": 11}})
-            return
-        else:
-            self.number_cache[message.guild.id] = {"number": num, "last": message.author.id}
-            await message.add_reaction(GREEN_CHECK)
-            collection.update_one({"_id": f"current_count_{message.guild.id}"}, {"$set": {"Number": num, "Last": message.author.id}})
+        async with self.lock:
+            msg = message.content.replace(",", "")
+            msg = msg.replace(" ", "")
+            try:
+                num = int(msg)
+            except:
+                return
+            last_count = self.number_cache.get(message.guild.id, {}).get("number", 0)
+            last_counter = self.number_cache.get(message.guild.id, {}).get("last", 11)
+            if num <= last_count:
+                await message.channel.send(f"{RED_X} {message.author.mention} RUINED IT at {num}!! The next number is **1**.")
+                await message.add_reaction(RED_X)
+                self.number_cache[message.guild.id] = {"number": 0, "last": 11}
+                collection.update_one({"_id": f"current_count_{message.guild.id}"}, {"$set": {"Number": 0, "Last": 11}})
+                return
+            elif message.author.id == last_counter:
+                await message.channel.send(f"{RED_X} {message.author.mention} RUINED IT at {num}!! You cannot count twice in a row! The next number is **1**.")
+                self.number_cache[message.guild.id] = {"number": 0, "last": 11}
+                await message.add_reaction(RED_X)
+                collection.update_one({"_id": f"current_count_{message.guild.id}"}, {"$set": {"Number": 0, "Last": 11}})
+                return
+            elif num - last_count > 1:
+                await message.channel.send(f"{RED_X} {message.author.mention} RUINED IT at {num}!! You cannot skip numbers! The next number is **1**.")
+                self.number_cache[message.guild.id] = {"number": 0, "last": 11}
+                await message.add_reaction(RED_X)
+                collection.update_one({"_id": f"current_count_{message.guild.id}"}, {"$set": {"Number": 0, "Last": 11}})
+                return
+            else:
+                self.number_cache[message.guild.id] = {"number": num, "last": message.author.id}
+                await message.add_reaction(GREEN_CHECK)
+                collection.update_one({"_id": f"current_count_{message.guild.id}"}, {"$set": {"Number": num, "Last": message.author.id}})
 
 def setup(bot):
     bot.add_cog(Counting(bot))
